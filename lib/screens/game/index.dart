56eb2b40f1eb7c181cce76b3bf9a24c0b623cf34
Wellington <wellingtonicoletti@gmail.com>
2024-10-19 13:04:46 -0300

    fix: correção reinicialização do jogo

--- lib/screens/game/index.dart
+++ lib/screens/game/index.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:get/instance_manager.dart';
 import 'package:get/state_manager.dart';
 import 'package:sudoku/screens/game/controller.dart';
@@ -13,55 +14,58 @@ class GameScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final controller = Get.put(GameController(nivel));
-    return Scaffold(
-      appBar: PreferredSize(
-        preferredSize: const Size.fromHeight(60),
-        child: Obx(
-          () => AppBar(
-            foregroundColor: Colors.white,
-            backgroundColor: Colors.transparent,
-            actions: [
-              IconButton(
-                onPressed: controller.gerarAnotacoes,
-                icon: controller.gerandoAnotacoes
-                    ? const CircularProgressIndicator()
-                    : const Icon(Icons.mode_edit_outlined),
-              ),
-              const SizedBox(width: 20)
-            ],
+    return PopScope(
+      onPopInvoked: (_) => Get.delete<GameController>(),
+      child: Scaffold(
+        appBar: PreferredSize(
+          preferredSize: const Size.fromHeight(60),
+          child: Obx(
+            () => AppBar(
+              foregroundColor: Colors.white,
+              backgroundColor: Colors.transparent,
+              actions: [
+                IconButton(
+                  onPressed: controller.gerarAnotacoes,
+                  icon: controller.gerandoAnotacoes
+                      ? const CircularProgressIndicator()
+                      : const Icon(Icons.mode_edit_outlined),
+                ),
+                const SizedBox(width: 20)
+              ],
+            ),
           ),
         ),
-      ),
-      body: SafeArea(
-        child: Obx(
-          () {
-            final isFinalizado = controller.jogoFinalizado;
-            if (controller.iniciandoJogo) {
-              return const Center(
-                child: CircularProgressIndicator(),
+        body: SafeArea(
+          child: Obx(
+            () {
+              final isFinalizado = controller.jogoFinalizado;
+              if (controller.iniciandoJogo) {
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+              return Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    const Text('Sudoku', style: TextStyle(fontSize: 35)),
+                    const SizedBox(height: 20),
+                    const SudokuInformacoes(),
+                    const SizedBox(height: 10),
+                    const SudokuGrid(),
+                    const SizedBox(height: 20),
+                    if (isFinalizado)
+                      ElevatedButton(
+                        onPressed: controller.reiniciar,
+                        child: const Text('Reiniciar'),
+                      )
+                    else
+                      const SudokuTeclado(),
+                  ],
+                ),
               );
-            }
-            return Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  const Text('Sudoku', style: TextStyle(fontSize: 35)),
-                  const SizedBox(height: 20),
-                  const SudokuInformacoes(),
-                  const SizedBox(height: 10),
-                  const SudokuGrid(),
-                  const SizedBox(height: 20),
-                  if (isFinalizado)
-                    ElevatedButton(
-                      onPressed: controller.reiniciar,
-                      child: const Text('Reiniciar'),
-                    )
-                  else
-                    const SudokuTeclado(),
-                ],
-              ),
-            );
-          },
+            },
+          ),
         ),
       ),
     );
