5f79a1dd8e275a1dcaf2c44cafdc78260724e370
WellingtonNico <wellingtonicoletti@gmail.com>
2025-03-05 08:52:47 -0300

    style: melhoria visual informações do jogo

--- lib/screens/game/informacoes.dart
+++ lib/screens/game/informacoes.dart
@@ -9,15 +9,31 @@ class SudokuInformacoes extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     GameController controller = Get.find();
+    final containerWidth = (MediaQuery.of(context).size.width - 40) * 0.333;
     return Obx(() {
       return Padding(
         padding: const EdgeInsets.symmetric(horizontal: 20),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Text('${controller.game.obterQuantidadeRevelados()}/81'),
-            Text('Nível ${controller.game.nivel}'),
-            Text('Erros ${controller.game.quantidadeDeErros}/3'),
+            SizedBox(
+              width: containerWidth,
+              child: Text('${controller.game.obterQuantidadeRevelados()}/81'),
+            ),
+            SizedBox(
+              width: containerWidth,
+              child: Text(
+                'Nível ${controller.game.nivel}',
+                textAlign: TextAlign.center,
+              ),
+            ),
+            SizedBox(
+              width: containerWidth,
+              child: Text(
+                'Erros ${controller.game.quantidadeDeErros}/3',
+                textAlign: TextAlign.end,
+              ),
+            ),
           ],
         ),
       );
